7b8764e6d4c173055514f4d1c15455cb89d24b6b
arcshock <frozenouthere@gmail.com>
2014-05-03 17:03:02 -0800

    Resturcture of the assets directory.
    Created a Monsters directory containing the scripts and models of the
    monsters.

---
+++ Assets/Monsters/Scripts/HelloWorldBehavior.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelloWorldBehavior : MonoBehaviour {
+	public string myName;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("(" + myName + ") I am alive and my position is " + transform.position);
+	}
+}
